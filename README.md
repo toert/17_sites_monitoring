ccdbeecd86e09093adade22dc2ae506c6a1a6114
toert <pw177@mail.ru>
2016-12-11 20:10:58 +0300

    first initial

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# 17_sites_monitoring
+﻿#Is my site working?
+
+##What does it do?
+
+The script check status of your sites(expiration date and status code)
+
+##How can I use it?
+
+You should compile it by Python 3.5:
+
+	python3.5 check_sites_health.py filepath_to_txt
+
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,59 @@
+import requests
+import whois
+from datetime import datetime, timedelta
+import argparse
+
+
+AMOUNT_DAYS = 30
+
+
+def parse_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',
+                        help='enter the filepath to file with URLs')
+    return parser.parse_args()
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as url_data:
+        return [url for url in url_data.read().splitlines()]
+
 
 def is_server_respond_with_200(url):
-    pass
+    return requests.get(url).status_code == 200
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    whois_obj = whois.whois(domain_name)
+    exper_datetime = whois_obj.expiration_date
+    if type(exper_datetime) is list:
+        return exper_datetime[0]
+    else:
+        return exper_datetime
+
+
+def is_it_pay_for_month(exp_date):
+    if exp_date is None:
+        return False
+    return (exp_date - datetime.today() > timedelta(days=AMOUNT_DAYS))
+
+
+def print_state(url, paid, available):
+    if paid and available:
+        print('{} is OK'.format(url))
+    elif not paid and available:
+        print('{} needs payments'.format(url))
+    elif paid and not available:
+        print('{} isn\'t available'.format(url))
+    else:
+        print('{} isn\'t available and needs payments'.format(url))
+
 
 if __name__ == '__main__':
-    pass
+    parser = parse_path()
+    filepath = parser.path
+    urls = load_urls4check(filepath)
+    urls = list(filter(lambda url: url, urls)) #delete empty lines
+    for url in urls:
+        exp = get_domain_expiration_date(url)
+        print_state(url, is_it_pay_for_month(exp), is_server_respond_with_200(url))
